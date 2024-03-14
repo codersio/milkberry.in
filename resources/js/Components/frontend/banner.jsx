08e5c7953f0fa897e2ad42d58820a988a35fa624
codersio <techtutorial727@gmail.com>
2024-03-14 12:45:34 +0530

    g

--- resources/js/Components/frontend/banner.jsx
+++ resources/js/Components/frontend/banner.jsx
@@ -11,7 +11,7 @@ const Banner = () => {
                         <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
                             <div className="p-3" style={{ maxWidth: '900px' }}>
                                 <h4 className="text-white text-uppercase mb-md-3">Traditional &amp; Delicious</h4>
-                                <h1 className="display-3 text-white mb-md-4">Traditional Ice Cream Since 1950</h1>
+                                <h1 className="display-3 text-white mb-md-4">Traditional Ice Cream Since 1850</h1>
                                 <a href className="btn btn-primary py-md-3 px-md-5 mt-2">Learn More</a>
                             </div>
                         </div>
